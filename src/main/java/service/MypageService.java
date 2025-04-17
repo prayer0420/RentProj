d761655e1253c0bfbe32b2da47ad0c6e6e4ad177
mokkmokk <ramona.april@gmail.com>
2025-04-17 12:36:41 +0900

    [chore] mypageService, Serviceimpl 생성

---
+++ src/main/java/service/MypageService.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+
+import dto.Product;
+import utils.PageInfo;
+
+public interface MypageService {
+
+	List<Product> productListByPage(PageInfo pageInfo) throws Exception;
+
+}
