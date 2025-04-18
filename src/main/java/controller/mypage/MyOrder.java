51c16419cf9d0d171c7b323c9d41838903b4aee1
mokkmokk <ramona.april@gmail.com>
2025-04-18 17:05:53 +0900

    [docs] MyOrder 서블릿 생성(작업중)

---
+++ src/main/java/controller/mypage/MyOrder.java
@@ -0,0 +1,58 @@
+package controller.mypage;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.Product;
+import service.mypage.MypageService;
+import service.mypage.MypageServiceImpl;
+import utils.PageInfo;
+
+/**
+ * Servlet implementation class MyOrder
+ */
+@WebServlet("/myOrder")
+public class MyOrder extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyOrder() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String pageStr = request.getParameter("page");
+		Integer page = null;
+		if(pageStr == null) {
+			page = 1;
+		}else {
+			page = Integer.parseInt(pageStr);
+		}
+		PageInfo pageInfo = new PageInfo(page);
+		MypageService service = new MypageServiceImpl();
+		try {
+//			List<Order> orderList = service.orderListByPage(pageInfo);
+			request.setAttribute("pageInfo", pageInfo);
+//			request.setAttribute("orderList", orderList);
+			request.getRequestDispatcher("/JSP/MyPage/myOrder.jsp").forward(request, response);
+		}catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("err",	"나의 판매상품 목록 조회를 실패했습니다.");
+			request.getRequestDispatcher("/JSP/MyPage/error.jsp").forward(request, response);
+		}
+	}
+
+}
